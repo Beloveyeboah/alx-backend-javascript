1855738ee21626110078b039b91175e448e69f33
beloveyeboah <beloveyeboah@gmail.com>
2024-10-03 13:13:54 -0700

    5. A Building

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,18 @@
+/* eslint-disable */
+export default class Building {
+    constructor(sqft) {
+        if (this.constructor === Building) {
+            throw new Error("Abstract class Building cannot be instantiated directly.");
+        }
+        this._sqft = sqft;
+    }
+
+    get sqft() {
+        return this._sqft;
+    }
+
+    evacuationWarningMessage() {
+        throw new Error("Class extending Building must override evacuationWarningMessage");
+    }
+}
+
